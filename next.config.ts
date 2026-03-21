b449dbfe56a85f21b9a0a1e8093793cf9f637a0e
pirate6969 <devsanjai0987@gmail.com>
2026-03-21 23:20:41 +0530

    fix: add cross-env for railway build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
-import path from "path";
-
 const nextConfig = {
   allowedDevOrigins: ["192.168.56.1"],
-  webpack: (config: any) => {
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@aptos-connect/wallet-adapter-plugin": path.resolve(
-        "./lib/aptos-connect-mock.js"
-      ),
-    };
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      config.resolve = config.resolve || {};
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@aptos-connect/wallet-adapter-plugin": false,
+      };
+    }
     return config;
   },
 };
