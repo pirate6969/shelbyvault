0048a0dddaac1716908442c7aa1d64730ed12ba0
pirate6969 <devsanjai0987@gmail.com>
2026-03-21 23:30:34 +0530

    fix: correct turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 const nextConfig = {
-  turbopack: {},
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        "@aptos-connect/wallet-adapter-plugin": { browser: "./lib/aptos-connect-mock.js" },
-      },
+  turbopack: {
+    resolveAlias: {
+      "@aptos-connect/wallet-adapter-plugin": "./lib/aptos-connect-mock.js",
     },
   },
 };
